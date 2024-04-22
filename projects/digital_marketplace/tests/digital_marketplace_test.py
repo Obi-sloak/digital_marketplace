3e99618b70d62c8a01096299b10e6d0b63e7154e
Obislok <odoemeneobinna@gmail.com>
2024-04-22 02:51:34 +0100

    test

--- projects/digital_marketplace/tests/digital_marketplace_test.py
+++ projects/digital_marketplace/tests/digital_marketplace_test.py
@@ -4,45 +4,53 @@ from algokit_utils import get_localnet_default_account
 from algokit_utils.config import config
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
+from algokit_utils.beta.algorand_client import AlgorandClient, PayParams, AssetCreateParams
+from algokit_utils.beta.account_manager import AddressAndSigner
 
 from smart_contracts.artifacts.digital_marketplace.client import DigitalMarketplaceClient
 
 
 @pytest.fixture(scope="session")
-def digital_marketplace_client(
-    algod_client: AlgodClient, indexer_client: IndexerClient
-) -> DigitalMarketplaceClient:
-    config.configure(
-        debug=True,
-        # trace_all=True,
-    )
+def algorand() -> AlgorandClient:
+    """Get an AlgorandClients to use through the test"""
+    return AlgorandClient.default_local_net()
 
+def dispenser(algorand: AlgodClient) -> AddressAndSigner:
+    """Get the dispenser to fund test addresses"""
+    return algorand.account.dispenser()
+
+@pytest.fixture(scope="session")
+def creator(algorand: AlgodClient, dispenser: AddressAndSigner) -> AddressAndSigner:
+    acct = algorand.account.random()
+
+    algorand.send.payment(PayParams(
+        sender= dispenser.address,
+        receiver=acct.address,
+        amount=10_000_000
+    ))
+
+    return acct
+
+@pytest.fixture(scope="session")
+def test_asset_id(creator: AddressAndSigner, algorand: AlgorandClient) -> int:
+    sent_txn = algorand.send.asset_create(
+        AssetCreateParams(sender=creator.address, total=10)
+        )
+    return sent_txn["confirmation"]["asset-index"]
+
+
+
+
+@pytest.fixture(scope="session")
+def digital_marketplace_clients(algorand: AlgorandClient, creator: AddressAndSigner, test_asset_id: int) -> DigitalMarketplaceClient:
+    """Instantiate an application client we can use for our tests"""
     client = DigitalMarketplaceClient(
-        algod_client,
-        creator=get_localnet_default_account(algod_client),
-        indexer_client=indexer_client,
-    )
+        algod_client=algorand.client.algod,
+        sender=creator.address,
+        signer=creator.signer,
+        )
 
-    client.deploy(
-        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
-        on_update=algokit_utils.OnUpdate.AppendApp,
-    )
-    return client
+    client.create_create_application(unitaryPrice=0, assetId=test_asset_id)
 
-
-def test_says_hello(digital_marketplace_client: DigitalMarketplaceClient) -> None:
-    result = digital_marketplace_client.hello(name="World")
-
-    assert result.return_value == "Hello, World"
-
-
-def test_simulate_says_hello_with_correct_budget_consumed(
-    digital_marketplace_client: DigitalMarketplaceClient, algod_client: AlgodClient
-) -> None:
-    result = (
-        digital_marketplace_client.compose().hello(name="World").hello(name="Jane").simulate()
-    )
-
-    assert result.abi_results[0].return_value == "Hello, World"
-    assert result.abi_results[1].return_value == "Hello, Jane"
-    assert result.simulate_response["txn-groups"][0]["app-budget-consumed"] < 100
+def test_pass(digital_marketplace_client: DigitalMarketplaceClient):
+    pass
